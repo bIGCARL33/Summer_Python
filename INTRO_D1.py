facb8726240304d511b9103238eb782bef86e02c
josh <josh@DESKTOP-24BE9FK.localdomain>
2020-05-31 13:19:23 -0500

    <Josh>

---
+++ INTRO_D1.py
@@ -0,0 +1,33 @@
+#This is a comment the hash tag means it is a comment does nothing
+print("Hello World")
+#The above is a string
+#Alt f12 to open terminal
+#tways to run Python
+#-terminal
+#_Pycharm (Right click and hit run)
+print("Test")
+#Ctrl +/ (Converts code to comments)(HIGLTED THough)
+x=7
+y=6
+z=x+y
+T=x^2+y
+print(T)
+print(z)
+
+
+#Quotations marks "" convert something to a strin+
+C = float(input("Farenheight"))
+#float converts your input into a decimal
+#Inputs are automatically considered as strings
+F=C*9/5+32
+C1=(F-32)*5/9
+Name=input("whats your name")
+print(C1)
+
+print(F)
+print(Name)
+
+# Variables
+#Assigment :make a basic caculator add subtract multiply divide and take usser input and display results
+D=8**2
+print(D)
